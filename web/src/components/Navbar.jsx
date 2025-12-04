bc472d95501ca4b5c6fd6503186ceabbfc5be732
tejasrastogileet <tejasrastogi456@gmail.com>
2025-12-04 11:08:40 +0530

    ok

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -5,9 +5,8 @@ export default function Navbar(){
   return (
     <nav className="nav">
       <div className="nav-inner container">
-        <div className="brand">GoTasks</div>
+    
         <div className="nav-actions">
-          <button className="btn btn-ghost">Login / Signup</button>
         </div>
       </div>
     </nav>
